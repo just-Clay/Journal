792f4a7e8ef14f727c642dc3b08cad85e168d77b
Connor Gable <gable.connor@gmail.com>
2026-09-22 20:27:27 -0600

    Text editing if finally working

--- src/main.py
+++ src/main.py
@@ -54,13 +54,13 @@ class JournalApplication(Adw.Application):
     def on_about_action(self, *args):
         """Callback for the app.about action."""
         about = Adw.AboutDialog(application_name='Journal',
-                                application_icon='io.github.just_Clay.Journal',
-                                developer_name='Connor Gable',
-                                version='0.1.0',
-                                # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
-                                translator_credits = _('translator-credits'),
-                                developers=['Connor Gable'],
-                                copyright='© 2026 Connor Gable')
+            application_icon='io.github.just_Clay.Journal',
+            developer_name='Connor Gable',
+            version='0.1.0',
+            # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
+            translator_credits = _('translator-credits'),
+            developers=['Connor Gable'],
+            copyright='© 2026 Connor Gable')
         about.present(self.props.active_window)
 
     def on_preferences_action(self, widget, _):
--- src/window.py
+++ src/window.py
@@ -46,7 +46,7 @@ class JournalWindow(Adw.ApplicationWindow):
     #Editor
     journal_entry=Gtk.Template.Child()
     bold_toggle=Gtk.Template.Child()
-    italics_toggle=Gtk.Template.Child()
+    italic_toggle=Gtk.Template.Child()
     header_selector=Gtk.Template.Child()
 
 
@@ -82,18 +82,23 @@ class JournalWindow(Adw.ApplicationWindow):
         self.buffer = self.journal_entry.get_buffer()
         self.bold = self.buffer.create_tag("bold", weight=Pango.Weight.BOLD)
         self.italic = self.buffer.create_tag("italic", style=Pango.Style.ITALIC)
-        self.header1 = self.buffer.create_tag("header1", scale=1.9, weight=Pango.Weight.SEMIBOLD)
-        self.header2 = self.buffer.create_tag("header2", scale=1.7, weight=Pango.Weight.SEMIBOLD)
-        self.header3 = self.buffer.create_tag("header3", scale=1.5, weight=Pango.Weight.SEMIBOLD)
-        self.header4 = self.buffer.create_tag("header4", scale=1.4, weight=Pango.Weight.SEMIBOLD)
-        self.header5 = self.buffer.create_tag("header5", scale=1.3, weight=Pango.Weight.SEMIBOLD)
-        self.header6 = self.buffer.create_tag("header6", scale=1.1, weight=Pango.Weight.SEMIBOLD)
-        self.is_updating_ui = False
+        self.header1 = self.buffer.create_tag("header1", scale=2.2, weight=Pango.Weight.SEMIBOLD)
+        self.header2 = self.buffer.create_tag("header2", scale=2, weight=Pango.Weight.SEMIBOLD)
+        self.header3 = self.buffer.create_tag("header3", scale=1.8, weight=Pango.Weight.SEMIBOLD)
+        self.header4 = self.buffer.create_tag("header4", scale=1.6, weight=Pango.Weight.SEMIBOLD)
+        self.header5 = self.buffer.create_tag("header5", scale=1.4, weight=Pango.Weight.SEMIBOLD)
+        self.header6 = self.buffer.create_tag("header6", scale=1.2, weight=Pango.Weight.SEMIBOLD)
+        self.updating_ui = False
+        self.current_tags = {"bold": False, "italic": False, "header": 0}
+        self.cursor_position = -1
+        self.old_length = self.buffer.get_char_count()
 
         #Editor buttons
         self.bold_toggle.connect("toggled", lambda _: self.on_bold_toggled())
-        self.italics_toggle.connect("toggled", lambda _: self.on_italic_toggled())
+        self.italic_toggle.connect("toggled", lambda _: self.on_italic_toggled())
         self.header_selector.connect("notify::selected", lambda *_: self.on_header_selected())
+        self.buffer.connect_after("insert-text", self.on_write)
+        self.buffer.connect_after("delete-range", self.after_delete)
         self.buffer.connect("notify::cursor-position", self.on_cursor_moved)
 
         #Making helpful variables for the calendar view
@@ -111,30 +116,74 @@ class JournalWindow(Adw.ApplicationWindow):
 
 
     #Editor view functions.
-    #TODO remember to implement continuous tag application while typing for bold and italic
     def on_bold_toggled(self):
-        is_active = self.bold_toggle.get_active()
+        #Stop if we are just updating ui
+        if self.updating_ui:
+            return
+        #Was an area selected when the toggle happened?
+        #If so, we are going to change the tags for that area based off of the
+        #starting tag of the selected area. Then we are going to adjust the
+        #current_tags and the toggle appropriately.
         if self.buffer.get_selection_bounds():
             start, end = self.buffer.get_selection_bounds()
-            if is_active:
+            if self.bold not in start.get_tags():
                 self.buffer.apply_tag(self.bold, start, end)
+                self.current_tags["bold"] = True
+                #Don't forget to update the button so it looks right
+                self.updating_ui = True
+                self.bold_toggle.set_active(True)
+                self.updating_ui = False
             else:
                 self.buffer.remove_tag(self.bold, start, end)
+                self.current_tags["bold"] = False
+                #Updating the button
+                self.updating_ui = True
+                self.bold_toggle.set_active(False)
+                self.updating_ui = False
+        #If no area was selected, we will just flip the toggle
+        else:
+            if self.current_tags["bold"]:
+                self.current_tags["bold"] = False
+            else:
+                self.current_tags["bold"] = True
 
     def on_italic_toggled(self):
-        is_active = self.italics_toggle.get_active()
+        if self.updating_ui:
+            return
+        #Was an area selected when the toggle happened?
+        #If so, we are going to change the tags for that area based off of the
+        #starting tag of the selected area. Then we are going to adjust the
+        #current_tags and the toggle appropriately.
         if self.buffer.get_selection_bounds():
             start, end = self.buffer.get_selection_bounds()
-            if is_active:
+            if self.italic not in start.get_tags():
                 self.buffer.apply_tag(self.italic, start, end)
+                self.current_tags["italic"] = True
+                #Don't forget to update the button so it looks right
+                self.updating_ui = True
+                self.italic_toggle.set_active(True)
+                self.updating_ui = False
             else:
                 self.buffer.remove_tag(self.italic, start, end)
+                self.current_tags["italic"] = False
+                #Updating the button
+                self.updating_ui = True
+                self.italic_toggle.set_active(False)
+                self.updating_ui = False
+        #If no area was selected, we will just flip the toggle
+        else:
+            if self.current_tags["italic"]:
+                self.current_tags["italic"] = False
+            else:
+                self.current_tags["italic"] = True
 
     def on_header_selected(self):
-        if self.is_updating_ui:
+        if self.updating_ui:
             return
+
         styles = [self.header1, self.header2, self.header3, self.header4, self.header5, self.header6]
         style_num = self.header_selector.get_selected()
+        self.current_tags["header"] = style_num
 
         #Select whole line
         if self.buffer.get_selection_bounds():
@@ -148,28 +197,123 @@ class JournalWindow(Adw.ApplicationWindow):
 
         for style in styles:
             self.buffer.remove_tag(style, start, end)
-        if style_num == 0:
-            pass
-        else:
+        if style_num != 0:
             self.buffer.apply_tag(styles[style_num - 1], start, end)
 
-    #Changes header button to match wherever cursor is
-    def on_cursor_moved(self, *args):
-        cursor = self.buffer.get_iter_at_mark(self.buffer.get_insert())
-        checker = cursor.copy()
-        if checker.ends_line() and not checker.starts_line():
-            checker.backward_char()
-        header_index = 0
-        header_tags = [self.header1, self.header2, self.header3, self.header4, self.header5, self.header6]
-        for i, tag in enumerate(header_tags):
-            if checker.has_tag(tag):
-                header_index = i + 1
-                break
-        if self.header_selector.get_selected() != header_index:
-            self.is_updating_ui = True
-            self.header_selector.set_selected(header_index)
-            self.is_updating_ui = False
+    #This fixes a funky header bug where you could get to different header tags
+    #on the same line by making the different tags on seperate lines
+    #and then merging them through deletion. We want to avoid this because
+    #we will be saving the files as markdown, which cannot have header text and
+    #non-header text in the same line.
+    def after_delete(self, buffer, start_deletion, end_deletion):
+        start = start_deletion.copy()
+        end = end_deletion.copy()
+        start.set_line_offset(0)
+        if not end.ends_line():
+            end.forward_to_line_end()
 
+        applied_tags = start.get_tags()
+        if self.bold in applied_tags:
+            applied_tags.remove(self.bold)
+        if self.italic in applied_tags:
+            applied_tags.remove(self.italic)
+        if applied_tags == []:
+            styles = [self.header1, self.header2, self.header3, self.header4, self.header5, self.header6]
+            for style in styles:
+                self.buffer.remove_tag(style, start, end)
+        else:
+            styles = [self.header1, self.header2, self.header3, self.header4, self.header5, self.header6]
+            for style in styles:
+                self.buffer.remove_tag(style, start, end)
+            self.buffer.apply_tag(applied_tags[0], start, end)
+
+    def on_write(self, buffer, location, text, length):
+        if "\n" in text:
+            self.current_tags["header"] = 0
+            self.updating_ui = True
+            self.header_selector.set_selected(0)
+            self.updating_ui = False
+
+        end = location.copy()
+        start = location.copy()
+        start.backward_chars(len(text))
+
+        if self.current_tags["bold"]:
+            buffer.apply_tag(self.bold, start, end)
+
+        if self.current_tags["italic"]:
+            buffer.apply_tag(self.italic, start, end)
+
+        if self.current_tags["header"] != 0:
+            headers = [self.header1, self.header2, self.header3, self.header4, self.header5, self.header6]
+            buffer.apply_tag(headers[self.current_tags["header"] - 1], start, end)
+
+    #When it moves, update the tags
+    #in current tags based on the surrounding tags at the new location. If there
+    #is no letters to the left of the cursor, use the letters to the right, but
+    #otherwise, base this off of the character to the left of where the cursor
+    #has moved
+    def on_cursor_moved(self, buffer, pspec):
+        cursor_offset = buffer.get_property("cursor-position")
+        character_count = self.buffer.get_char_count()
+
+        if (cursor_offset - 1 != self.cursor_position or character_count - 1 != self.old_length) and character_count != 0:
+            #We must have jumped or deleted something
+            location = buffer.get_iter_at_offset(cursor_offset)
+            target = location.copy()
+            if not target.starts_line():
+                target.backward_char()
+            applied_tags = target.get_tags()
+            read_tags = []
+            for tag in applied_tags:
+                read_tags.append(tag.get_property("name"))
+
+            self.updating_ui = True
+
+            if "bold" in read_tags:
+                self.current_tags["bold"] = True
+                self.bold_toggle.set_active(True)
+                read_tags.remove("bold")
+            else:
+                self.current_tags["bold"] = False
+                self.bold_toggle.set_active(False)
+            if "italic" in read_tags:
+                self.current_tags["italic"] = True
+                self.italic_toggle.set_active(True)
+                read_tags.remove("italic")
+            else:
+                self.current_tags["italic"] = False
+                self.italic_toggle.set_active(False)
+
+            if read_tags == []:
+                self.current_tags["header"] = 0
+                self.header_selector.set_selected(0)
+            else:
+                header = read_tags[0]
+                if header == "header1":
+                    self.current_tags["header"] = 1
+                    self.header_selector.set_selected(1)
+                elif header == "header2":
+                    self.current_tags["header"] = 2
+                    self.header_selector.set_selected(2)
+                elif header == "header3":
+                    self.current_tags["header"] = 3
+                    self.header_selector.set_selected(3)
+                elif header == "header4":
+                    self.current_tags["header"] = 4
+                    self.header_selector.set_selected(4)
+                elif header == "header5":
+                    self.current_tags["header"] = 5
+                    self.header_selector.set_selected(5)
+                elif header == "header6":
+                    self.current_tags["header"] = 6
+                    self.header_selector.set_selected(6)
+
+
+            self.updating_ui = False
+
+        self.cursor_position = cursor_offset
+        self.old_length = character_count
 
     #Update the month year selector to reflect correct month and year in calendar view
     def update_month_year(self):
